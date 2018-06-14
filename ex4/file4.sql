642de23b1725ca0b1091d7b8d1217a92a6c3866f
Doina <atiniod3@gmail.com>
2018-06-13 21:22:44 -0600

    commit new indexes file

---
+++ ex4/file4.sql
@@ -0,0 +1,42 @@
+# Create indexes for the tables
+# ------------------------------------------------------------
+
+# Index for Categories Table
+
+CREATE UNIQUE INDEX index_id
+ON categories (id);
+
+# Index for Country Table
+
+CREATE UNIQUE INDEX index_id
+ON country (id);
+
+# Index for Expenses Table
+
+CREATE UNIQUE INDEX index_id
+ON expenses (id);
+
+# Index for Language Table
+
+CREATE UNIQUE INDEX index_id
+ON language (id);
+
+# Index for Profesions Table
+
+CREATE UNIQUE INDEX index_id
+ON profesions (id);
+
+# Index for Salary Table
+
+CREATE UNIQUE INDEX index_id
+ON salary (id);
+
+# Index for user_lang Table
+
+CREATE UNIQUE INDEX index_id
+ON user_lang (id);
+
+# Index for Users Table
+
+CREATE UNIQUE INDEX index_id
+ON users (id);
